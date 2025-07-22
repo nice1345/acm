7f04a44450056cc6c1cd8d8d3af61d827d38259f
nice1345 <3181609465@qq.com>
2025-07-22 22:27:49 +0800

    20250722

--- main.cpp
+++ main.cpp
@@ -6,42 +6,7 @@ const int N = 2e5 + 5;
 const int mod = 998244353;
 
 void solve() {
-    int n, k;
-    cin >> n >> k;
-    vector<vector<pair<int,int>>> g(n, vector<pair<int,int>>());
-    vector<vector<int>>vis(n,vector<int>(k,2));
-    vector<vector<bool>>fr(n*2 - 2,vector<bool>(k,false));
-    vector<int>f(n,-1);
-    for (int i = 0; i < n - 1; ++i) {
-        int u, v;
-        cin >> u >> v;
-        --u;
-        --v;
-        g[u].emplace_back(v,i*2);
-        g[v].emplace_back(u,i*2|1);
-    }
-    f[0] = 0;
-    queue<array<int, 4>> q;
-    q.push({0, 0, 0, 0});
-    while (!q.empty()) {
-        auto [cur, fa, b, d] = q.front();\
-        q.pop();
-        ++d;
-        b = (b + 1) % k;
-        for (auto [to,e]: g[cur]) {
-            if (vis[to][b] == 0 || (b != 1 && to == fa) || fr[e][b])continue;
-            --vis[to][b];
-            if (!fr[e][b]) {
-                fr[e][b]=true;
-                if(b == 0 && f[to] == -1){
-                    f[to] = d/k;
-                }
-            }
-            q.push({to, cur, b, d});
-        }
-    }
-    for (int i = 1; i < n; ++i)cout << f[i] << ' ';
-    cout << '\n';
+
 }
 
 signed main() {
